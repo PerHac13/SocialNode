ca667d5f7644868dbe6b5b25e35bffc8bae78cd6
PerHac13 <abdullah13112004@gmail.com>
2025-02-03 13:40:50 +0530

    feat (post): server.js
    entry point integration

---
+++ post-service/src/server.js
@@ -0,0 +1,43 @@
+require("dotenv").config();
+
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+
+const connectDB = require("./config/db");
+const routing = require("./middleware/routing");
+const {
+  sensitiveEndpointsLimiter,
+  RedisClientMiddleware,
+} = require("./middleware/rateLimiter");
+const errorHandler = require("./middleware/errorHandler");
+
+const routes = require("./routes/post_routes");
+const logger = require("./utils/logger");
+
+const app = express();
+const PORT = process.env.PORT || 4002;
+
+// Connect to MongoDB
+connectDB();
+
+// Middleware
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(routing);
+
+app.use(sensitiveEndpointsLimiter);
+// Routes
+app.use("/api/posts", RedisClientMiddleware, routes);
+
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  logger.info(`Post services running on port ${PORT}`);
+});
+
+// unhandled promise rejections
+process.on("unhandledRejection", (reason, promise) => {
+  logger.error(`Unhandled Rejection at: ${promise} reason: ${reason}`);
+});
