1ffc5f492cc54fe3030af6a24c0001545d7102d0
PerHac13 <abdullah13112004@gmail.com>
2025-02-08 00:49:53 +0530

    chore (search): utils

---
+++ search-service/src/utils/rabbitmq.js
@@ -0,0 +1,52 @@
+const amqp = require("amqplib");
+const logger = require("./logger");
+
+let connection = null;
+let channel = null;
+
+const EXCHANGE_NAME = process.env.RABBITMQ_EXCHANGE_NAME;
+
+async function connectToRabbitMQ() {
+  try {
+    connection = await amqp.connect(process.env.RABBITMQ_URL);
+    channel = await connection.createChannel();
+
+    await channel.assertExchange(EXCHANGE_NAME, "topic", { durable: false });
+    logger.info("Connected to rabbit mq");
+    return channel;
+  } catch (err) {
+    logger.error("Error connecting to rabbit mq", err);
+  }
+}
+
+async function publishEvent(routingKey, message) {
+  if (!channel) {
+    await connectToRabbitMQ();
+  }
+
+  channel.publish(
+    EXCHANGE_NAME,
+    routingKey,
+    Buffer.from(JSON.stringify(message))
+  );
+  logger.info(`Event published: ${routingKey}`);
+}
+
+async function consumeEvent(routingKey, callback) {
+  if (!channel) {
+    await connectToRabbitMQ();
+  }
+
+  const q = await channel.assertQueue("", { exclusive: true });
+  await channel.bindQueue(q.queue, EXCHANGE_NAME, routingKey);
+  channel.consume(q.queue, (msg) => {
+    if (msg !== null) {
+      const content = JSON.parse(msg.content.toString());
+      callback(content);
+      channel.ack(msg);
+    }
+  });
+  logger.info(`Subscribed to event and consumed it: ${routingKey}`);
+}
+
+module.exports = { connectToRabbitMQ, publishEvent, consumeEvent };
